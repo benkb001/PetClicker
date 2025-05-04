9e2e953e89b5ea3b94d1bee702d33e9ccb621e5c
benkb001 <benkb01@umd.edu>
2025-05-04 09:06:58 -0400

    test4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app); 
-server.listen(5000); 
+server.listen(process.env.PORT || 5000); 
 console.log("Server started");
